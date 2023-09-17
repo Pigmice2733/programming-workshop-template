569480fae5cc7d983656e06e842466867ee8bd76
LucaHaverty <lucahaverty@gmail.com>
2023-09-17 11:06:20 -0700

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
