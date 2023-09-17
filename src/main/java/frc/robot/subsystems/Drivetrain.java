1a5c543684a85af01e1fcb1753cd392ca5fedb10
LucaHaverty <lucahaverty@gmail.com>
2023-09-17 11:27:07 -0700

    Installed REVLib and improved subsystem comments

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,19 +2,24 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// This is a comment. Any lines of code starting with "//" are for writing notes
+// about the code, and will be ignored when the code is executing
+
+// This is a subsystem class. It corresponds to a specific part of the robot (this one if for the Drivetrain)
 public class Drivetrain extends SubsystemBase {
     double exampleVariable = 27.33;
 
     public Drivetrain() {
-        // This is the constructor. Any code in this function will be called when the
-        // robot is turned on
+        // This is the constructor. Any code in this function will be run only once
+        // as soon as the robot is turned on
 
     }
 
     @Override
     public void periodic() {
-        // Any code in this function will be called repeatedly while the robot is
-        // enabled
+        // Any code in this function will be run repeatedly while the robot is
+        // enabled Similar to the "repeat forever" block in scratch or Update function
+        // in Unity
 
     }
 }
