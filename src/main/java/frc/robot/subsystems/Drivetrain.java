e78d269280ebdff7fcc816dabfc5b7d1861794a6
LucaHaverty <lucahaverty@gmail.com>
2023-09-17 11:12:45 -0700

    Finsihed drivetrain template, removed not needed code

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,8 +3,18 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    double exampleVariable = 27.33;
+
+    public Drivetrain() {
+        // This is the constructor. Any code in this function will be called when the
+        // robot is turned on
+
+    }
+
+    @Override
+    public void periodic() {
+        // Any code in this function will be called repeatedly while the robot is
+        // enabled
+
+    }
 }
