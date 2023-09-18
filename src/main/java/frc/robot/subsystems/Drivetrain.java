1722fdd36cb37b781c31164455f6417e068e9906
Elijah Vennebush <92764232+ev118@users.noreply.github.com>
2023-09-17 20:45:38 -0700

    changes to subsystem file for style and comprehensiveness

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,56 +6,54 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// This is a comment. Any lines of code starting with "//" are for writing notes
-// about the code, and will be ignored when the code is executing
-
-// This is a subsystem class. It corresponds to a specific part of the robot (this one if for the Drivetrain)
 public class Drivetrain extends SubsystemBase {
-    double exampleVariable = 27.33;
+    // double exampleVariable = 27.33;
 
-    double forwardSpeedMultiplier = 0.3;
-    double turnSpeedMultiplier = 0.3;
+    double forwardSpeed, turnSpeed;
+    double forwardSpeedMultiplier, turnSpeedMultiplier;
+    double leftOutput, rightOutput;
+    boolean boostEnabled, slowEnabled;
+    CANSparkMax leftMotor, rightMotor;
+    XboxController controller;
 
-    CANSparkMax leftMotor = new CANSparkMax(11, MotorType.kBrushless);
-    CANSparkMax rightMotor = new CANSparkMax(12, MotorType.kBrushless);
-
-    XboxController controller = new XboxController(0);
-
+    /* Creates a new Drivetrain subsystem. */
     public Drivetrain() {
-        // This is the constructor. Any code in this function will be run only once
-        // as soon as the robot is turned on
+        forwardSpeed = turnSpeed = 0;
+        
+        forwardSpeedMultiplier = turnSpeedMultiplier = 0.3;
+        
+        leftOutput = rightOutput = 0;
 
+        boostEnabled = slowEnabled = false;
+        
+        leftMotor = new CANSparkMax(11, MotorType.kBrushless);
+        rightMotor = new CANSparkMax(12, MotorType.kBrushless);
+        
+        controller = new XboxController(0);
     }
 
     @Override
     public void periodic() {
-        // Any code in this function will be run repeatedly while the robot is
-        // enabled Similar to the "repeat forever" block in scratch or Update function
-        // in Unity
-
         // Get forward and turn speed from controllers
-        double forwardSpeed = controller.getLeftY() * forwardSpeedMultiplier;
-        double turnSpeed = controller.getRightX() * turnSpeedMultiplier;
+        forwardSpeed = controller.getLeftY() * forwardSpeedMultiplier;
+        turnSpeed = controller.getRightX() * turnSpeedMultiplier;
 
         // Calculate left and right motor speeds
-        double leftOutput = forwardSpeed + turnSpeed;
-        double rightOutput = forwardSpeed - turnSpeed;
+        leftOutput = forwardSpeed + turnSpeed;
+        rightOutput = forwardSpeed - turnSpeed;
 
         // Boost mode
-        boolean boostEnabled = controller.getXButtonPressed();
+        boostEnabled = controller.getXButtonPressed();
 
-        if (boostEnabled == true) {
+        if (boostEnabled) {
             leftOutput = leftOutput * 1.5;
             rightOutput = rightOutput * 1.5;
         }
 
         // Slow mode
-        boolean slowEnabled = controller.getYButtonPressed();
-
-        if (slowEnabled == true) {
-            leftOutput = leftOutput * 0.5;
-            rightOutput = rightOutput * 0.5;
-        }
+        slowEnabled = controller.getYButtonPressed();
+        leftOutput *= slowEnabled ? 0.5 : 1;
+        rightOutput *= slowEnabled ? 0.5 : 1;
 
         // Set the motor outputs
         leftMotor.set(leftOutput);
